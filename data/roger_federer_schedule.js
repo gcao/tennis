e603fc062261538bfc9ca4815c2941ce5bdf991a
Guoliang Cao <gcao99@gmail.com>
2014-01-23 21:54:43 -0500

    Update code and data

---
+++ data/roger_federer_schedule.js
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9"> <![endif]-->
+<html class='no-js'>
+  <head>
+    <meta charset='utf-8'>
+    <meta content='Tennis for fans,Tennis fans,Guoliang Cao,Tennis,ATP,ATP 1000,Grand Slam,Roger Federer,Novak Djokovic,Rafael Nadal,Andy Murray,David Ferrer,Del Potro,Berdych,Tsonga,Ranking,Tennis players,Schedule,Tournaments,Rank history,Win,Loss,Chart' name='keywords'>
+    <meta content='IE=edge,chrome=1' http-equiv='X-UA-Compatible'>
+    <title>TennisForFans.com</title>
+    <meta name="viewport" content="initial-scale=1,user-scalable=no,maximum-scale=1.0">
+    <script src='javascripts/vendor/modernizr-2.6.1.min.js'></script>
+    <link href='stylesheets/site.css' rel='stylesheet'>
+    <script>
+      (function() {
+      
+        window.useLocalData = false;
+      
+        window.jQuery || document.write('<' + 'script src="javascripts/vendor/jquery-1.8.0.min.js"><' + '/script>');
+      
+        window.ticketUrls = [
+          {
+            "name": "US Open",
+            "url": "http://www.ticketcity.com/tennis-tickets/us-open-tennis-tickets.html"
+          }, {
+            "name": "Coupe Rogers",
+            "url": "http://www.ticketcity.com/tennis-tickets/rogers-cup-mens-tennis-canada-tickets.html"
+          }, {
+            "name": "Western & Southern Open",
+            "url": "http://www.ticketcity.com/tennis-tickets/western-and-southern-open-tickets.html"
+          }, {
+            "name": "Australian Open",
+            "url": "http://www.ticketcity.com/tennis-tickets/australian-open-tennis-tickets.html"
+          }, {
+            "name": "BNP Paribas Open",
+            "url": "http://www.ticketcity.com/tennis-tickets/australian-open-tennis-tickets.html"
+          }, {
+            "name": "Sony Open Tennis",
+            "url": "http://www.ticketcity.com/tennis-tickets/sony-ericsson-open-tennis-tickets.html"
+          }, {
+            "name": "Wimbledon",
+            "url": "http://www.ticketcity.com/tennis-tickets/wimbledon-tennis-tickets.html"
+          }
+        ];
+      
+      }).call(this);
+    </script>
+  </head>
+  <body>
+    <!--[if lt IE 7]> <p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p> <![endif]-->
+    <header>
+      <div class='wrap'>
+        <h2>
+          <a href='index.html'>
+            <img class='logo' src='images/site-logo.gif'>
+          </a>
+        </h2>
+        <ul id='nav'>
+          <li class='rankings'>
+            <a href='#/rankings'>Rankings</a>
+          </li>
+          <li class='rank-history'>
+            <a href='javascript:void(0)'>Rank History</a>
+            <ul>
+              <li>
+                <a href='#/rank-history/novak_djokovic,roger_federer,rafael_nadal,andy_murray'>BIG FOUR</a>
+              </li>
+              <li>
+                <a href='#/rank-history/roger_federer'>Roger Federer</a>
+              </li>
+              <li>
+                <a href='#/rank-history/rafael_nadal'>Rafael Nadal</a>
+              </li>
+              <li>
+                <a href='#/rank-history/novak_djokovic'>Novak Djokovic</a>
+              </li>
+              <li>
+                <a href='#/rank-history/andy_murray'>Andy Murray</a>
+              </li>
+            </ul>
+          </li>
+          <li class='games'>
+            <a href='javascript:void(0)'>Games</a>
+            <ul>
+              <li>
+                <a href='#/games/roger_federer'>Roger Federer</a>
+              </li>
+              <li>
+                <a href='#/games/rafael_nadal'>Rafael Nadal</a>
+              </li>
+              <li>
+                <a href='#/games/novak_djokovic'>Novak Djokovic</a>
+              </li>
+              <li>
+                <a href='#/games/andy_murray'>Andy Murray</a>
+              </li>
+              <li>
+                <a href='#/games/david_ferrer'>David Ferrer</a>
+              </li>
+            </ul>
+          </li>
+          <li class='win-loss'>
+            <a href='javascript:void(0)'>Win/Loss</a>
+            <ul>
+              <li>
+                <a href='#/win-loss/novak_djokovic,roger_federer,rafael_nadal,andy_murray'>BIG FOUR</a>
+              </li>
+              <li>
+                <a href='#/win-loss/roger_federer'>Roger Federer</a>
+              </li>
+              <li>
+                <a href='#/win-loss/rafael_nadal'>Rafael Nadal</a>
+              </li>
+              <li>
+                <a href='#/win-loss/novak_djokovic'>Novak Djokovic</a>
+              </li>
+              <li>
+                <a href='#/win-loss/andy_murray'>Andy Murray</a>
+              </li>
+              <li>
+                <a href='#/win-loss/david_ferrer'>David Ferrer</a>
+              </li>
+              <li>
+                <a href='#/win-loss/tomas_berdych'>Tomas Berdych</a>
+              </li>
+              <li>
+                <a href='#/win-loss/juan_martin_del_potro'>Juan Martin Del Potro</a>
+              </li>
+              <li>
+                <a href='#/win-loss/jo_wilfried_tsonga'>Jo-Wilfried Tsonga</a>
+              </li>
+            </ul>
+          </li>
+          <li class='current tournaments'>
+            <a href='javascript:void(0)'>Schedules</a>
+            <ul>
+              <li>
+                <a href='#/tournaments'>Tournaments</a>
+              </li>
+              <li>
+                <a href='#/schedule/roger_federer'>Roger Federer</a>
+              </li>
+              <li>
+                <a href='#/schedule/rafael_nadal'>Rafael Nadal</a>
+              </li>
+              <li>
+                <a href='#/schedule/novak_djokovic'>Novak Djokovic</a>
+              </li>
+              <li>
+                <a href='#/schedule/andy_murray'>Andy Murray</a>
+              </li>
+            </ul>
+          </li>
+          <li class='support'>
+            <a href='support.html'>Support</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+    <div class='main' role='main'>
+      (function() {
+      
+        ({
+          _id: "roger_federer_schedule",
+          data: [
+            {
+              tournament: "Australian Open",
+              result: "QF",
+              defeated: "Jo-Wilfried Tsonga etc"
+            }, "ABN AMRO World Tennis Tournament", "BNP Paribas Open", "Mutua Madrid Open", "Internazionali BNL d'Italia", "Roland Garros", "Gerry Weber Open", "Wimbledon", "Rogers Cup", "Western & Southern Open", "US Open", "Shanghai Rolex Masters", "BNP Paribas Masters"
+          ]
+        });
+      
+      }).call(this);
+    </div>
+    <footer></footer>
+    <script src='javascripts/site.js'></script>
+    <script>
+      if (!location.hostname.match('localhost')) {
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-38153915-1']);
+        _gaq.push(['_trackPageview']);
+      
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+      }
+    </script>
+  </body>
+</html>
